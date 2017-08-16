678b2392a650c68c35f45f831878fe69aab6f4bc
simonhlee97 <simonhlee97@hotmail.com>
2017-08-16 17:10:31 -0400

    for loops

--- 0.07_loops.js
+++ 0.07_loops.js
@@ -1,3 +1,15 @@
+// GOLD:
+// Create a for loop that counts to 10.
+// Print each number, 1 through 8.
+// But print 9 & 10 as "nine" & "ten"
+
+
 for (i=1; i<=10; i++) {
-	
+	if (i<9) {
+		console.log(i)
+	} else if (i<10) {
+		console.log ("nine")
+	} else {
+		console.log ("ten")
+	}
 }
